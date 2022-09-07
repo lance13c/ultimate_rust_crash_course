6acc0d7af373a64636f36571d526472d3dde2cc9
Dominic Cicilio <dominic@yoamigo.io>
2022-09-07 02:09:34 -0400

    converts if statement to match statement

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -10,9 +10,21 @@
 //
 // You will need to complete 1b as well before you will be able to run this program successfully.
 
+enum Shot {
+  Bullseye,
+  Hit(f64),
+  Miss
+}
+
 impl Shot {
     // Here is a method for the `Shot` enum you just defined.
     fn points(self) -> i32 {
+      match self {
+        Shot::Bullseye => 5,
+        Shot::Hit(x) if x < 3.0 => 2,
+        Shot::Hit(x) => 1,
+        Shot::Miss => 0
+      }
         // 1b. Implement this method to convert a Shot into points
         // - return 5 points if `self` is a `Shot::Bullseye`
         // - return 2 points if `self` is a `Shot::Hit(x)` where x < 3.0
@@ -30,13 +42,27 @@ fn main() {
     //
     //   A. Call `coord.print_description()`
     //   B. Append the correct variant of `Shot` to the `shots` vector depending on the value of
-    //   `coord.distance_from_center()`
+
+    for coord in arrow_coords {
+      let shot = match coord.distance_from_center() {
+        distance if distance > 5.0 => Shot::Miss,
+        distance if distance >= 1.0 => Shot::Hit(distance),
+        _ => Shot::Bullseye,
+      };
+
+      shots.push(shot);
+    }
+    
     //      - Less than 1.0 -- `Shot::Bullseye`
     //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
     //      - Greater than 5.0 -- `Shot::Miss`
 
 
     let mut total = 0;
+
+    for shot in shots {
+      total += shot.points();
+    }
     // 3. Finally, loop through each shot in shots and add its points to total
 
     println!("Final point total is: {}", total);
